62761607732a090906a6e4e49eaea0fc97a7fdce
ikswary <ikswary@naver.com>
2020-04-24 11:42:52 +0900

    add : product detail view and url settings both in root, product app

--- product/views.py
+++ product/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views import View
 
-# Create your views here.
+from product.models import Product
+
+class DetailView(View):
+    def get(self, request):
+        try:
+            target = request.GET.get('product')
+            info = dict()
+            product = Product.objects.prefetch_related('nutrient_set__size',
+                                                       'productallergycauses_set__allergy_causes',
+                                                       'milkselection_set__milk').get(name_eng=target)
+
+            menu = {
+                'name_kor': product.name_kor,
+                'name_eng': product.name_eng,
+                'description': product.description,
+            }
+
+            if product.temperature is not '':
+                info['구분'] = product.temperature
+            if len(product.milkselection_set.all()) > 0:
+                info['우유 선택'] = list()
+                for milk in product.milkselection_set.all():
+                    info['우유 선택'].append(milk.milk.name)
+            if len(product.productallergycauses_set.all()) > 0:
+                info['알레르기 유발물질'] = list()
+                for allergy_id in product.productallergycauses_set.all():
+                    info['알레르기 유발물질'].append(allergy_id.allergy_causes.name)
+            if len(product.nutrient_set.all()) > 0:
+                info['제공사이즈'] = list()
+                for size_id in product.nutrient_set.all():
+                    info['제공사이즈'].append(size_id.size.name)
+
+            nutrient_list = []
+            for nutrient in product.nutrient_set.all():
+                keys = list(nutrient.__dict__.keys())
+                values = list(nutrient.__dict__.values())
+                nutrients = dict()
+                for i in range(4, len(keys) - 1):
+                    nutrients[keys[i]] = values[i]
+                nutrient_list.append(nutrients)
+
+            return JsonResponse({'menu': menu, 'info': info, 'nutrient': nutrient_list})
+
+        except Product.DoesNotExist:
+            return HttpResponse(status=404)
+
+
