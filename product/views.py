cb1f743b6a089e681a27e317895599860e2997fe
ikswary <ikswary@naver.com>
2020-04-25 14:08:22 +0900

    update : made Menuview to response in order of is_new, is_best attr

--- product/views.py
+++ product/views.py
@@ -1,26 +1,29 @@
 from django.http import JsonResponse, HttpResponse
 from django.views import View
 
-from product.models import Menu, Category
+from product.models import Menu
 
-def get_products_in_json(target):
-    return [{
-        'name_kor': product.name_kor,
-        'name_eng': product.name_eng,
-        'images': [image.url for image in product.image_set.all()],
-        'is_new': product.is_new,
-        'is_best': product.is_best,
-    } for product in target.product_set.all()]
 
 class MenuView(View):
+    def get_products_in_json(self, target):
+        return [
+            {
+                'name_kor': product.name_kor,
+                'name_eng': product.name_eng,
+                'images': [image.url for image in product.image_set.all()],
+                'is_new': product.is_new,
+                'is_best': product.is_best,
+            } for product in target.product_set.order_by('-is_new', '-is_best')
+        ]
+
     def get(self, request, menu, category=None):
         try:
             target = Menu.objects.prefetch_related('category_set__product_set__image_set').get(name=menu)
             if category:
-                result_list = get_products_in_json(target.category_set.get(id=category))
+                result_list = self.get_products_in_json(target.category_set.get(id=category))
                 return JsonResponse({'products': result_list}, status=200, json_dumps_params={'ensure_ascii': False})
 
-            result_list = get_products_in_json(target)
+            result_list = self.get_products_in_json(target)
             return JsonResponse({'products': result_list}, status=200, json_dumps_params={'ensure_ascii': False})
 
         except KeyError:
