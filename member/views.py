9d1a4247708e43d5624612b79a4e151b1a12bf73
Magrfs <magnolirfsit@gmail.com>
2020-04-27 15:46:27 +0900

    Fix views

--- member/views.py
+++ member/views.py
@@ -7,46 +7,63 @@ from django.views import View
 from django.http import HttpResponse, JsonResponse
 
 from .models import User
-from paulbassett.settings import SECRET_KEY, ALGORITHM
 
 
 class UserView(View):
+    def validate_id(self, user_id):
+        if User.objects.filter(user_id=user_id).exists():
+            return False
+        return True
+
+    def validate_pw(self, password):
+        if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@.#^* ?+=_~])[A-Za-z\d!@.#^* ?+=_~]{8,}$", password):
+            return False
+        return True
+
+    def validate_name(self, name):
+        if name is None or len(name) == 0:
+            return False
+        return True
+
+    def validate_birth_date(self, birth_date):
+        if birth_date is None or len(birth_date) == 0:
+            return False
+        if not re.match(r"(\d{4})-(\d{2})-(\d{2})", birth_date):
+            return False
+        return True
+
+    def validate_phone(self, phone):
+        if User.objects.filter(phone=phone).exists():
+            return False
+        if phone is None or len(phone) == 0:
+            return False
+        return True
+
+    def validate_email(self, email):
+        if email is None or len(email) == 0 or not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
+            return False
+        if User.objects.filter(email=email).exists():
+            return False
+        return True
+
     def post(self, request):
-        data = json.loads(request.body)
-        print(data)
         try:
-            if len(data.keys()) == 1:
-                if len(re.findall(r"[ㄱ-ㅎ가-힣ㅏ-ㅣ&$ %\s]", data['user_id'])) > 0 or len(data['user_id']) == 0:
-                    return JsonResponse({"message": "INVALID_ID"}, status=400)
-
-                if User.objects.filter(user_id=data['user_id']).exists():
-                    return JsonResponse({"message": "DUPLICATE_ID"}, status=409)
-                return HttpResponse(status=200)
-
-            if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@.#^* ?+=_~])[A-Za-z\d!@.#^* ?+=_~]{8,}$", data['password']):
-                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
+            data = json.loads(request.body)
             hashed_password = bcrypt.hashpw(
                 data['password'].encode('utf-8'), bcrypt.gensalt())
 
-            if data['name'] is None or len(data['name']) == 0:
-                return JsonResponse({"message": "INPUT_NAME"}, status=400)
+            VALIDATION_RULES = {
+                'user_id': self.validate_id,
+                'password': self.validate_pw,
+                'name': self.validate_name,
+                'birth_date': self.validate_birth_date,
+                'phone': self.validate_phone,
+                'email': self.validate_email,
+            }
 
-            if data['birth_date'] is None or len(data['birth_date']) == 0:
-                return JsonResponse({"message": "INPUT_DATE"}, status=400)
-
-            if not re.match(r'(\d{4})-(\d{2})-(\d{2})', data['birth_date']):
-                return JsonResponse({"message": "INVALID_TIME"}, status=400)
-
-            if User.objects.filter(phone=data['phone']).exists():
-                return JsonResponse({"message": "DUPLICATE_PHONE_NUMBER"}, status=409)
-
-            if data['phone'] is None or len(data['phone']) == 0:
-                return JsonResponse({"message": "INPUT_NUMBER"}, status=400)
-
-            if data['email'] is None or len(data['email']) == 0 or not re.match(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', data['email']):
-                return JsonResponse({"message": "INPUT_MAIL_ADDRESS"}, status=400)
-            if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({"message": "DUPLICATE_EMAIL"}, status=400)
+            for field, validator in VALIDATION_RULES.items():
+                if not validator(data[field]):
+                    return HttpResponse(status=400)
 
             User.objects.create(
                 user_id=data['user_id'],
