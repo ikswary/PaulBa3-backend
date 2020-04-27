0a65e53541370385a80becf8f8ed4f566ad23d8b
ikswary <ikswary@naver.com>
2020-04-27 20:23:05 +0900

    fix : category index bug
    fix : Menuview to return just one image
    correct : typo between image and images

--- product/views.py
+++ product/views.py
@@ -10,7 +10,7 @@ class MenuView(View):
             {
                 'name_kor': product.name_kor,
                 'name_eng': product.name_eng,
-                'images': [image.url for image in product.image_set.all()],
+                'image': product.image_set.first().url,
                 'is_new': product.is_new,
                 'is_best': product.is_best,
             } for product in target.product_set.order_by('-is_new', '-is_best')
@@ -18,7 +18,7 @@ class MenuView(View):
 
     def get(self, request, menu, category=None):
         try:
-            target = Menu.objects.prefetch_related('category_set__product_set__image_set').get(name=menu)
+            target = Menu.objects.prefetch_related('category_set__product_set__image_set').all()[category - 1]
             if category:
                 result_list = self.get_products_in_json(target.category_set.get(id=category))
                 return JsonResponse({'products': result_list}, status=200, json_dumps_params={'ensure_ascii': False})
