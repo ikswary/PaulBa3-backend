ba280fe66a192d8357a25cc3a9bba67bfc7f3cbb
ikswary <ikswary@naver.com>
2020-04-28 19:50:28 +0900

    add : url settings for BranchView

--- branch/urls.py
+++ branch/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from branch.views import BranchView
 
 urlpatterns = [
-
+    path('/detail', BranchView.as_view()),
+    path('/detail/<str:target_code>', BranchView.as_view())
 ]
