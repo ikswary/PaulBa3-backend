8fbce5ee57218e1fa68d412af0f89cfc06b46240
Magrfs <magnolirfsit@gmail.com>
2020-04-28 13:33:36 +0900

    Fix views and Add UserCheck endpoint urls

--- member/views.py
+++ member/views.py
@@ -5,69 +5,51 @@ import re
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
+from django.db import IntegrityError
 
 from .models import User
 
 
+class UserCheckView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if len(re.findall(r"[ㄱ-ㅎ가-힣ㅏ-ㅣ&$ %\s]", data['user_id'])) > 0 or len(data['user_id']) == 0:
+                return JsonResponse({"message": "INVALID_ID"}, status=400)
+
+            if User.objects.filter(user_id=data['user_id']).exists():
+                return JsonResponse({"message": "DUPLICATE_ID"}, status=409)
+            return HttpResponse(status=200)
+
+        except KeyError:
+            return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+
 class UserView(View):
-    def validate_id(self, user_id):
-        if User.objects.filter(user_id=user_id).exists():
-            return False
-        return True
-
-    def validate_pw(self, password):
-        if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@.#^* ?+=_~])[A-Za-z\d!@.#^* ?+=_~]{8,}$", password):
-            return False
-        return True
-
-    def validate_name(self, name):
-        if name is None or len(name) == 0:
-            return False
-        return True
-
-    def validate_birth_date(self, birth_date):
-        if birth_date is None or len(birth_date) == 0:
-            return False
-        if not re.match(r"(\d{4})-(\d{2})-(\d{2})", birth_date):
-            return False
-        return True
-
-    def validate_phone(self, phone):
-        if User.objects.filter(phone=phone).exists():
-            return False
-        if phone is None or len(phone) == 0:
-            return False
-        return True
-
-    def validate_email(self, email):
-        if email is None or len(email) == 0 or not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
-            return False
-        if User.objects.filter(email=email).exists():
-            return False
-        return True
+    VALIDATION_RULES = {
+        'user_id': lambda user_id: False if User.objects.filter(user_id=user_id).exists() else True,
+        'password': lambda password: False if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@.#^* ?+=_~])[A-Za-z\d!@.#^* ?+=_~]{8,}$", password) else True,
+        'name': lambda name: False if name is None or len(name) == 0 else True,
+        'birth_date': lambda birth_date: False if not re.match(r"(\d{4})-(\d{2})-(\d{2})", birth_date) else True,
+        'phone': lambda phone: False if User.objects.filter(phone=phone).exists() else True,
+        'email': lambda email: False if not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email) else True
+    }
 
     def post(self, request):
         try:
             data = json.loads(request.body)
-            hashed_password = bcrypt.hashpw(
-                data['password'].encode('utf-8'), bcrypt.gensalt())
+            if len(data.keys()) < 6:
+                return HttpResponse(status=400)
+            for value in data.values():
+                if value in "":
+                    return HttpResponse(status=400)
 
-            VALIDATION_RULES = {
-                'user_id': self.validate_id,
-                'password': self.validate_pw,
-                'name': self.validate_name,
-                'birth_date': self.validate_birth_date,
-                'phone': self.validate_phone,
-                'email': self.validate_email,
-            }
-
-            for field, validator in VALIDATION_RULES.items():
+            for field, validator in self.VALIDATION_RULES.items():
                 if not validator(data[field]):
                     return HttpResponse(status=400)
 
             User.objects.create(
                 user_id=data['user_id'],
-                password=hashed_password.decode('utf-8'),
+                password=bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
                 name=data['name'],
                 birth_date=data['birth_date'],
                 phone=data['phone'],
@@ -76,5 +58,7 @@ class UserView(View):
             )
             return HttpResponse(status=200)
 
+        except IntegrityError:
+            return JsonResponse({"message": "DUPLICATED_KEYS"}, status=400)
         except KeyError:
             return JsonResponse({"message": "INVALID_KEYS"}, status=400)
