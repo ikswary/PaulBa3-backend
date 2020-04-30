9cad442bd4be6b3b948b7c0b568aa7812d4d345c
Magrfs <magnolirfsit@gmail.com>
2020-04-30 13:30:42 +0900

    Add branchArea endpoint

--- branch/urls.py
+++ branch/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from branch.views import BranchView
+from branch.views import BranchView, AreaView
 
 urlpatterns = [
     path('/detail', BranchView.as_view()),
-    path('/detail/<str:target_code>', BranchView.as_view())
+    path('/detail/<str:target_code>', BranchView.as_view()),
+    path('/<str:target_code>', AreaView.as_view())
 ]
--- branch/views.py
+++ branch/views.py
@@ -39,3 +39,21 @@ class BranchView(View):
 
         except ObjectDoesNotExist:
             return HttpResponse(status=404)
+
+    class AreaView(View):
+        def get(self, request, target_code=''):
+            try:
+                region = Region.objects.prefetch_related('area_set__branch_set').get(code=target_code)
+
+                area_info = [
+                    {
+                        "area_name": area.name.strip(),
+                        "area_code": area.code,
+                        "clickable": area.branch_set.exists()
+                    } for area in region.area_set.all()
+                ]
+                area_info.insert(0, {"area_name": "전체", "area_code": target_code+"00", "clickable": True})
+
+                return JsonResponse({"area_info": area_info}, status=200)
+            except Region.DoesNotExist:
+                return HttpResponse(status=404)
